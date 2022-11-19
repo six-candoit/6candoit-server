cdf462379dab932775082db4b185ab2c0cea5835
holmir97 <holmir97@gmail.com>
2022-11-19 22:31:37 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import router from "./router";
+import connectDB from "./loader/db";
+import config from "./config";
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+// Port Host
+const PORT: number = config.port;
+
+// route
+app.use("/", router);
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "production" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+const server = app
+  .listen(PORT, () => {
+    console.log(
+      `
+    ##############################################
+    🛡️  Server listening on port: ${PORT} 🛡️
+    ##############################################
+  `
+    );
+  })
+  .on("error", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+server.timeout = 1000000;
