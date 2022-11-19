f3966e97b15d30bfb7ed29e7c90df0e5a3298542
holmir97 <holmir97@gmail.com>
2022-11-20 07:22:50 +0900

    최신화

--- src/router/content.ts
+++ src/router/content.ts
@@ -1,11 +1,119 @@
 import { Router } from "express";
-import { dummyData } from "../constants/dummy";
 // Middleware
 import { authMiddleware } from "../middlewares";
 import { Request, Response } from "express";
 // Controller
 
-import { reportController } from "../controller";
+const dummyData = {
+  Good: [
+    {
+      content: "새우를 먹기 좋게 야무지게 까줬다",
+      point: 1,
+    },
+    {
+      content: "내 커피 취향을 기억해줬다",
+      point: 1,
+    },
+    {
+      content: "잠들 때까지 통화해줬다 ",
+      point: 2,
+    },
+    {
+      content: "추운 겨울날 핫팩을 챙겨줬다",
+      point: 2,
+    },
+    {
+      content: "버스 탈 때 기사님께 인사를 했다",
+      point: 3,
+    },
+    {
+      content: "치킨 닭다리를 두 개 다 양보해줬다",
+      point: 3,
+    },
+    {
+      content: "특별한 날이 아닌데 편지를 써왔다",
+      point: 4,
+    },
+    {
+      content: "아플 때 문 앞에 약을 두고 갔다",
+      point: 4,
+    },
+    {
+      content: "기분 안 좋은 날 엽떡 시켜줬다",
+      point: 5,
+    },
+    {
+      content: "날이 좋아서 꽃을 사왔다",
+      point: 5,
+    },
+  ],
+  Bad: [
+    {
+      content: "잔다고 말 안하고 잠들었다",
+      point: -1,
+    },
+    {
+      content: "카톡 말투 바뀌었다고 틱틱댈 때",
+      point: -1,
+    },
+    {
+      content: "메뉴 고를 때 아무거나 좋다고 했다 ",
+      point: -2,
+    },
+    {
+      content: "차 안에서 머리끈이 발견되었다",
+      point: -2,
+    },
+    {
+      content: "잔다고 했는데 인스타 초록불일 때",
+      point: -3,
+    },
+    {
+      content: "화났는데 왜 화났는지 얘기를 안 한다",
+      point: -3,
+    },
+    {
+      content: "호의가 계속되면 권리인 줄 안다",
+      point: -4,
+    },
+    {
+      content: "은연 중에 전애인과 비교했다",
+      point: -4,
+    },
+    {
+      content: "나몰래 다른 이성과 술을 마셨다",
+      point: -5,
+    },
+    {
+      content: "애인 방에서 발견한 낯선 머리카락",
+      point: -5,
+    },
+  ],
+};
+
+//"새우를 먹기 좋게 야무지게 까줬다" (1)
+// 내 커피 취향을 기억해줬다 (1)
+// 잠들 때까지 통화해줬다 (2)
+// 추운 겨울날 핫팩을 챙겨줬다 (2)
+// 버스 탈 때 기사님께 인사를 했다 (3)
+// 치킨 닭다리를 두 개 다 양보해줬다 (3)
+// 특별한 날이 아닌데 편지를 써왔다 (4)
+// 아플 때 문 앞에 약을 두고 갔다 (4)
+// 기분 안 좋은 날 엽떡 시켜줬다 (5)
+// 날이 좋아서 꽃을 사왔다 (5)
+
+// Bad
+
+// 잔다고 말 안하고 잠들었다 (-1)
+// 카톡 말투 바뀌었다고 틱틱댈 때(-1)
+// 메뉴 고를 때 아무거나 좋다고 했다 (-2)
+// 차 안에서 머리끈이 발견되었다 (-2)
+// 잔다고 했는데 인스타 초록불일 때 (-3)
+// 화났는데 왜 화났는지 얘기를 안 한다 (-3)
+//  호의가 계속되면 권리인 줄 안다 (-4)
+// 은연 중에 전애인과 비교했다 (-4)
+// 나몰래 다른 이성과 술을 마셨다 (-5)
+// 애인 방에서 발견한 낯선 머리카락 (-5)
 
 const router: Router = Router();
 
@@ -13,11 +121,11 @@ router.get("", async (req: Request, res: Response) => {
   const { type } = req.query;
   if (Number(type) == 1) {
     const data = dummyData;
-    return data.Good;
+    return res.status(200).json(data.Good);
   }
   if (Number(type) == 0) {
     const data = dummyData;
-    return data.Bad;
+    return res.status(200).json(data.Bad);
   }
 });
 
